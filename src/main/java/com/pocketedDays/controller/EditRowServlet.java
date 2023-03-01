a0a020390b0d29069d5785a28bebc7112b4dcd8d
Myo Min Han <mhan3@madisoncollege.edu>
2023-03-02 01:07:51 +0630

    Add controller for editRow jsp

---
+++ src/main/java/com/pocketedDays/controller/EditRowServlet.java
@@ -0,0 +1,53 @@
+package com.pocketedDays.controller;
+
+import com.pocketedDays.entity.Row;
+import com.pocketedDays.persistence.RowDao;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.time.LocalDate;
+
+@WebServlet (
+        urlPatterns = {"/editRow"}
+)
+public class EditRowServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String submit = request.getParameter("submit");
+        int rowId = Integer.parseInt(request.getParameter("rowId"));
+        HttpSession session = request.getSession();
+        int sheetId = (int) session.getAttribute("sheetId");
+        int rowCreatorId = (int) session.getAttribute("userId");
+        if (submit.equals("Edit Row")) {
+            RowDao rowDao = new RowDao();
+            Row row = rowDao.getRowsByRowId(rowId);
+            row.setSheetId(sheetId);
+            row.setRowCreatorId(rowCreatorId);
+            row.setCreatedDate(LocalDate.now());
+            row.setRowDescription(request.getParameter("rowDescription"));
+            //row.setQuantity(Integer.parseInt(request.getParameter("quantity")));
+            row.setCostPerItem(Integer.parseInt(request.getParameter("costPerItem")));
+            row.setRowType(request.getParameter("rowType"));
+            row.setTag(request.getParameter("tag"));
+
+            rowDao.saveOrUpdateRow(row);
+
+            RequestDispatcher dispatcher = request.getRequestDispatcher("/viewRow?sheetId=" + sheetId);
+            dispatcher.forward(request, response);
+        } else {
+            RowDao rowDao = new RowDao();
+            Row row = rowDao.getRowsByRowId(rowId);
+            request.setAttribute("row", row);
+            request.setAttribute("sheetId", sheetId);
+
+            RequestDispatcher dispatcher = request.getRequestDispatcher("/editRow.jsp");
+            dispatcher.forward(request, response);
+        }
+    }
+}
